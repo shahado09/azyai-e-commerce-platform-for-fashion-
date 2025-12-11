8f2741ca8728717f126e7309d7ae51ae450789a6
shahado09 <almurran.shahd@gmail.com>
2025-12-11 21:13:30 +0300

    finish edit

--- controllers/cloth.js
+++ controllers/cloth.js
@@ -4,7 +4,7 @@ const Cloth =require("../models/cloth")
 const multer = require('multer');
 const path = require('path');
 
-
+// mutler
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join('public', 'uploads'));
@@ -13,13 +13,12 @@ const storage = multer.diskStorage({
     cb(null, `${Date.now()}-${file.originalname}`); // Unique filename
   }
 });
-
 const upload = multer({ storage });
 const multiUpload = upload.fields([
   { name: "images", maxCount: 3}
 ])
 
-
+// index
 router.get('/',async(req,res)=>{
     try{
         const allCloth= await Cloth.find()
@@ -29,12 +28,12 @@ router.get('/',async(req,res)=>{
     res.send("Error loading clothes");}
 })
 
-
+// new
 router.get('/new',(req,res)=>{
     res.render('cloth/new.ejs')
 })
 
-
+// create
 router.post('/',multiUpload,async(req,res)=>{
 
     try{ 
@@ -63,6 +62,7 @@ router.post('/',multiUpload,async(req,res)=>{
 })
 
 
+// show
 router.get('/:id',async (req,res)=>{
 
   console.log(req.params.id)
@@ -70,14 +70,48 @@ router.get('/:id',async (req,res)=>{
     res.render('cloth/show.ejs',{foundCloth})
 })
 
+
+// update
+router.get('/:id/edit', async (req, res) => {
+  const foundCloth = await Cloth.findById(req.params.id);
+  res.render('cloth/edit.ejs', { foundCloth });
+});
+router.put('/:id',multiUpload, async (req,res)=>{
+  
+  try{
+  if (req.body.isAvailable){req.body.isAvailable=true}
+  else{ req.body.isAvailable=false}
+
+  const updateData = {
+  name: req.body.name,
+  description: req.body.description,
+  sizes: req.body.sizes,
+  isAvailable: req.body.isAvailable};
+ 
+
+  if (req.files && req.files["images"] && req.files["images"].length > 0) {
+    updateData.images = req.files["images"].map(file => '/uploads/' + file.filename);}
+
+    
+  const updated = await Cloth.findByIdAndUpdate( req.params.id,updateData,{ new: true });
+
+   res.redirect('/cloth/' + updated._id);
+  }
+
+  catch(err){
+        console.log(err)
+        res.render('cloth/new.ejs', { errorMessage: 'Something went wrong. Please try again.' });
+    }
+})
+
+
+
+// delete
 router.delete('/:id', async (req, res) => {
   await Cloth.findByIdAndDelete(req.params.id)
   res.redirect('/cloth')
 })
 
-router.get('/:id/edit', async (req,res)=>{
-  const foundCloth =await Cloth.findById(req.params.id)
-    res.render('cloth/edit.ejs',{foundCloth})
-})
+
 
 module.exports = router;
