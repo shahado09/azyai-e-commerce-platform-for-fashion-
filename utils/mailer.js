13dd85f11ac481fe287a2d09d9187c4cea6a343d
shahado09 <almurran.shahd@gmail.com>
2025-12-15 18:12:11 +0300

    nodemailer send req res

--- utils/mailer.js
+++ utils/mailer.js
@@ -17,3 +17,28 @@ async function sendVendorRequestToAdmin({userEmail, userName, instagram, vendort
     await transporter.sendMail({ from: process.env.EMAIL_USER, to: process.env.ADMIN_EMAIL,subject,text,});
 }
 
+
+async function sendDecisionToUser(toEmail, userName, decision, adminNote) {
+  let subject;
+  let text;
+
+  if (decision === "approved") {
+    subject = "Your Vendor Request Approved";
+    text = `Hi ${userName},
+    Your vendor request has been approved
+    You can now create/edit your products.
+    Thanks,
+    Azyai Team`;} 
+
+  else {
+        subject = " Your Vendor Request Rejected";
+        text = `Hi ${userName},
+
+        Your vendor request has been rejected 
+        Admin note: ${adminNote ? adminNote : "No note provided"}
+        You can submit a new request later.
+        Thanks,
+        Azyai Team`;}
+
+        await transporter.sendMail({ from: process.env.EMAIL_USER, to: toEmail, subject: subject, text: text, });
+}
