3986ccba679d68b6d69675ed71edbbe623681f08
shahado09 <almurran.shahd@gmail.com>
2025-12-16 12:09:40 +0300

    update vendor rej req

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,13 +19,54 @@ router.get("/vendor-requests", isAdmin, async (req, res) => {
     res.status(400).send("Error approving request");
   }});
 
-// update vendor req router 
-router.put('/vendor-requists',isAdmin ,async(req,res)=>{
+// vendor approve req router 
+router.put('/vendor-requists/:id/approve',isAdmin ,async(req,res)=>{
 
   try{
     const createdRequest =  await VendorRequest.findById(req.params.id).populate("userId");
+    if(!createdRequest){return res.status(404).send("Request not found");}
 
-  }
+    createdRequest.status="approved";
+    adminNote="";
+    await  save();
+
+
+    await User.findByIdAndUpdate(createdRequest.userId._id,{role:"vendor"})
+
+    await sendDecisionToUser({
+      toEmail:createdRequest.userId.email, 
+      userName:createdRequest.userId.username, 
+      decision:'approved', 
+      adminNote:'',
+    });
+    res.redirect("/admin/vendor-requests");}
+
+  catch (err) {
+    console.error(err);
+    res.status(400).send("Error approving request");}
+})
+
+// vendor reject req router 
+router.put('/vendor-requists/:id/reject',isAdmin ,async(req,res)=>{
+
+  try{
+    const createdRequest =  await VendorRequest.findById(req.params.id).populate("userId");
+    if(!createdRequest){return res.status(404).send("Request not found");}
+
+    createdRequest.status="rejected";
+    adminNote=req.body.adminNote || "there is no note";
+    await  save();
+
+
+    await User.findByIdAndUpdate(createdRequest.userId._id,{role:"vendor"})
+
+    await sendDecisionToUser({
+      toEmail:createdRequest.userId.email, 
+      userName:createdRequest.userId.username, 
+      decision:'rejected', 
+      adminNote:createdRequest.adminNote,
+    });
+    res.redirect("/admin/vendor-requests");}
 
   catch (err) {
     console.error(err);
